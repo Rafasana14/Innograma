cb07ceab1d19dfa0b30ce7cdad22be7ef289f660
Ivan <ivan.moreno.granado@gmail.com>
2022-11-12 01:38:18 +0100

    code/task-008):Estilo y paginación al listado
    Se le ha añadido el estilo al listado de ponencias y la funcionalidad de paginación, establecida a 3 registros por página

--- Innograma/urls.py
+++ Innograma/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from api.api.router import router_api
-from api.views import get_all_conferencias
+from api.views import ConferenciasView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router_api.urls)),
-    path("ponencias/",get_all_conferencias,name="ponencias"),
+    path("ponencias/",ConferenciasView.as_view(),name="ponencias"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Conferencia, Evento, Ponente
-
+from django.views.generic import ListView
 
 # Create your views here.
 
-def get_all_conferencias(request):
-    conferencias = Conferencia.objects.all()
-    return render(request, 'ponencias.html', {'conferencias': conferencias})
+class ConferenciasView(ListView):
+    template_name = 'ponencias.html'
+    model = Conferencia
+    paginate_by = 3
+    context_object_name = 'conferencias'
+
