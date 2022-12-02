529a2ed5e81f45c97888320e1988c2c0122c7171
Miguel Ángel Rivas Rosado <migrivros@alum.us.es>
2022-12-02 10:19:41 +0100

    code(task-028): Test CRUD de ponencias
    Se han agregado tests de casos negativos que comprueba que los campos que no pueden ser blancos lo sean

--- testdb/test_ponencias.py
+++ testdb/test_ponencias.py
@@ -14,7 +14,7 @@ class PonenciaTestCase(TestCase):
     def setUp(self):
         Conferencia.objects.create(tema="Redes",fecha="2022-10-05 12:00",espacio="ETSII",aforo_max="200",n_asistentes="75")
         Conferencia.objects.create(tema="Redes To Update",fecha="2022-10-05 12:00",espacio="ETSII",aforo_max="200",n_asistentes="75")
-        
+
     def test_ponencia_create(self):
         ponencia = Conferencia.objects.get(tema = "Redes")
         self.assertIsNotNone(ponencia)
@@ -46,22 +46,37 @@ class PonenciaTestCase(TestCase):
         self.assertEqual(ponencia_prueba.espacio,"A1.30")
         self.assertEqual(ponencia_prueba.aforo_max,300)
         self.assertEqual(ponencia_prueba.n_asistentes,100)
-
+    
 
 
     # CASOS NEGATIVOS
-     
+   
     def test_ponencia_create_null_tema(self):
         with self.assertRaises(Exception):
             Conferencia.objects.create(tema=None,fecha=FECHA,espacio=ESPACIO,aforo_max=AFORO,n_asistentes=ASISTENTES)
+    
+    def test_ponencia_create_blank_tema(self):
+            conferencia = Conferencia(tema="",fecha=FECHA,espacio=ESPACIO,aforo_max=AFORO,n_asistentes=ASISTENTES)
+            with self.assertRaises(ValidationError):
+                conferencia.full_clean()
 
     def test_ponencia_create_null_fecha(self):
         with self.assertRaises(Exception):
             Conferencia.objects.create(tema=TEMA,fecha=None,espacio=ESPACIO,aforo_max=AFORO,n_asistentes=ASISTENTES)
 
+    def test_ponencia_create_blank_fecha(self):
+            conferencia = Conferencia(tema=TEMA,fecha="",espacio=ESPACIO,aforo_max=AFORO,n_asistentes=ASISTENTES)
+            with self.assertRaises(ValidationError):
+                conferencia.full_clean()
+
     def test_ponencia_create_null_espacio(self):
         with self.assertRaises(Exception):
             Conferencia.objects.create(tema=TEMA,fecha=FECHA,espacio=None,aforo_max=AFORO,n_asistentes=ASISTENTES)
+    
+    def test_ponencia_create_blank_espacio(self):
+            conferencia = Conferencia(tema=TEMA,fecha=FECHA,espacio="",aforo_max=AFORO,n_asistentes=ASISTENTES)
+            with self.assertRaises(ValidationError):
+                conferencia.full_clean()
     
     def test_ponencia_create_error_format_fecha(self):
         with self.assertRaises(Exception):
@@ -82,4 +97,4 @@ class PonenciaTestCase(TestCase):
     def test_ponencia_update_error_format_fecha(self):
         with self.assertRaises(Exception):
             Conferencia.objects.update(tema=TEMA,fecha="10-02-2021 00:00",espacio=ESPACIO,aforo_max=AFORO,n_asistentes=ASISTENTES)
-
+  
